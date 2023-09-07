a5512ec10fa3352cc5ccdcff67ebb5dae49c254e
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2023-09-07 14:58:36 +0800

    建置：+ Tauri 並初始化

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,18 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            tauri::async_runtime::spawn(async move {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                main_window.show().unwrap();
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
