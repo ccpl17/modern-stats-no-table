fbd3ac0e7cd1763e9b89e8e10f8d9dca96d4283f
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2023-10-04 17:59:56 +0800

    重構：main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,10 +7,9 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let main_window = app.get_window("main").unwrap();
-            tauri::async_runtime::spawn(async move {
-                std::thread::sleep(std::time::Duration::from_secs(1));
-                main_window.show().unwrap();
-            });
+
+            main_window.show().unwrap();
+
             Ok(())
         })
         .run(tauri::generate_context!())
