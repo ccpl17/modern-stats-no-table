d959377504c832adb1f946af025252710f2b4e76
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2023-10-05 12:26:40 +0800

    修正 (Android)：閃退問題

--- src-maui/MainPage.xaml.cs
+++ src-maui/MainPage.xaml.cs
@@ -14,23 +14,29 @@ public partial class MainPage
     const string currentVersion = "23.10.0";
     const string endpointUrl = "https://gist.githubusercontent.com/ccpl17/0330e04f234b987104345bebd510695d/raw/";
 
-    var client = new HttpClient();
-    var response = await client.GetAsync(endpointUrl);
-
-    var jsonContent = response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
-    var data = jsonContent != null ? JsonSerializer.Deserialize<Dictionary<string, string>>(jsonContent) : null;
-    var remoteVersion = data != null
-      ? data.TryGetValue("version", out var versionValue) ? versionValue : currentVersion
-      : currentVersion;
-
-    var updateNow = Version.Parse(currentVersion) < Version.Parse(remoteVersion)
-      ? await DisplayAlert("有可用的更新", "你想要現在下載嗎", "好 ", "取消")
-      : false;
-
-    if (updateNow)
+    try
     {
-      var uri = new Uri("https://github.com/ccpl17/stat-crit-value-calculator/releases");
-      await Browser.Default.OpenAsync(uri, BrowserLaunchMode.External);
+      var client = new HttpClient();
+      var response = await client.GetAsync(endpointUrl);
+
+      var jsonContent = response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
+      var data = jsonContent != null ? JsonSerializer.Deserialize<Dictionary<string, string>>(jsonContent) : null;
+      var remoteVersion = data != null
+        ? data.TryGetValue("version", out var versionValue) ? versionValue : currentVersion
+        : currentVersion;
+
+      var updateNow = Version.Parse(currentVersion) < Version.Parse(remoteVersion)
+        ? await DisplayAlert("有可用的更新", "你想要現在下載嗎", "好 ", "取消")
+        : false;
+
+      if (updateNow)
+      {
+        var uri = new Uri("https://github.com/ccpl17/stat-crit-value-calculator/releases");
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.External);
+      }
+    }
+    catch
+    {
     }
   }
 }
